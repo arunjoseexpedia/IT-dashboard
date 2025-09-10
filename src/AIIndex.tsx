977db3c7679b36d967a55253548dc21a57f98508
null <null>
2025-09-11 00:02:24 +0530

    test

--- src/AIIndex.tsx
+++ src/AIIndex.tsx
@@ -13,8 +13,8 @@ const AIIndex = () => {
           
           
             <div>
-              <div style={{ fontSize: '15px', fontWeight: 'bold', color:'#FFA500' }}>AIIndex</div>
-             <div style={{display:'flex',marginLeft:'25px'}}> <div style={{ fontSize: '30px' }}>0.4 MM </div>&nbsp;<div style={{marginTop:'15px',padding:'2px',height:'20px',fontSize:'13px',background:'#E9142B',color: 'white'}}><i>savings</i></div></div>
+              <div style={{ fontSize: '15px', fontWeight: 'bold'  }}>AIIndex</div>
+             <div style={{display:'flex',marginLeft:'25px', color:'#FFA500'}}> <div style={{ fontSize: '30px' }}>0.4 MM </div>&nbsp;<div style={{marginTop:'15px',padding:'2px',height:'20px',fontSize:'13px',background:'#E9142B',color: 'white'}}><i>savings</i></div></div>
               <div style={{ fontSize: '10px' }}>AIIndex</div>
             </div>
           </div>
