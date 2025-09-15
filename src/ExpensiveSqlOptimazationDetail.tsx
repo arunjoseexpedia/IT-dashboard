46be93bf521b164d5403f402dbc43992e895411f
null <null>
2025-09-16 01:44:19 +0530

    test

--- src/ExpensiveSqlOptimazationDetail.tsx
+++ src/ExpensiveSqlOptimazationDetail.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import Topbar from './Topbar';
 import { useNavigate } from 'react-router-dom';
+import { Link } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
 
 export const ExpensiveSqlOptimazationDetail = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>hai</h1>
-      <button onClick={() => navigate('/')}>Back to Dashboard</button>
+    <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
+      {/* Header */}
+      <header style={{ height: '10%', borderRadius:'8px' }}>
+        <Topbar />
+      </header>
+      <div style={{ padding: '5px', alignSelf: 'flex-start'}}>
+      <Link component={RouterLink} to="/" underline="hover" color="primary">
+  Back
+</Link>
+      </div>
     </div>
   );
 };
