3f322e4b23f9fd844f7bf9ce55ff8dcbdfb3677e
Arun <arun.konkoth@ust.com>
2026-07-27 10:48:19 +0530

    test

--- src/components/ApplicationStatusChart.tsx
+++ src/components/ApplicationStatusChart.tsx
@@ -143,6 +143,7 @@ const ApplicationStatusChart: React.FC<ApplicationStatusChartProps> = ({ data, t
         >
           <ResponsiveContainer width="100%" height="100%">
             <BarChart
+              tabIndex={0}
               data={data}
               margin={{ top: 2, right: 8, left: 0, bottom: 0 }}
             >
